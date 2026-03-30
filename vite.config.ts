204810a6f0a3385e4ae2ebdd5725b9f31b0b8e0f
redstar <redstar@RedStar>
2026-03-30 03:07:28 +0200

    cleanNux

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,14 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'kimi-plugin-inspect-react'
+
 export default defineConfig({
-  base: './',
+  base: '/Nexus/', 
   plugins: [inspectAttr(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
